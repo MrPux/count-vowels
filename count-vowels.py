dde5221669247ea6c50b6f72b155aeb8a3e84228
MrPuxe <anthondeleonsketch@gmail.com>
2023-12-25 19:32:53 -0500

    Added comment next to print statement # prints 3. Also added removed the str() encapuslation of i and count True booleans instead of True strings

--- count-vowels.py
+++ count-vowels.py
@@ -1,5 +1,5 @@
 #   count_vowels("programming")  # Should return 3
 def count_vowels(string):
-    return [(str(i.lower() in ['a', 'e', 'i', 'o', 'u'])) for i in list(string)].count('True')
+    return [(i.lower() in ['a', 'e', 'i', 'o', 'u']) for i in list(string)].count(True)
 
-print(count_vowels("programming"))
+print(count_vowels("programming")) # prints 3
