4dd4183a75c155c38ca4dc00aedfda351377d9c1
MrPuxe <anthondeleonsketch@gmail.com>
2023-12-25 19:28:45 -0500

    coded count_vowels() function

--- count-vowels.py
+++ count-vowels.py
@@ -1,1 +1,5 @@
 #   count_vowels("programming")  # Should return 3
+def count_vowels(string):
+    return [(str(i.lower() in ['a', 'e', 'i', 'o', 'u'])) for i in list(string)].count('True')
+
+print(count_vowels("programming"))
